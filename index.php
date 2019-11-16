7754332f0ef99b3a36bb4443ef452e9ab99ac9ef
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-16 15:12:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<script type="text/javascript">
-		let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  ...
-});
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  showInstallPromotion();
-});
-		btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-});
-		window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 76 and later from showing the mini-infobar
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  showInstallPromotion();
-});
-		window.addEventListener('appinstalled', (evt) => {
-  console.log('a2hs installed');
-});
 	</script>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="/package.json">
